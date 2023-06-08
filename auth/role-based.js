04a5c9c78c599f1b40ba4f20fc5dc889856d8dd8
aashrikaa <134730633+aashrikaa@users.noreply.github.com>
2023-06-08 21:46:45 +0545

    Update role-based.js

--- auth/role-based.js
+++ auth/role-based.js
@@ -1,88 +1,39 @@
-import express from "express";
-import loadDb from "./loadDb"; // dummy middleware to load db (sets request.db)
-import authenticate from "./authentication"; // middleware for doing authentication
-import permit from "./permission"; // middleware for checking if user's role is permitted to make request
+const express = require('express');
+const mysql = require('mysql');
 
-const app = express(),
-      api = express.Router();
+const app = express();
 
-// first middleware will setup db connection
-app.use(loadDb);
+// Create a MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'dummy_db'
+});
 
-// authenticate each request
-// will set `request.user`
-app.use(authenticate);
-
-// setup permission middleware,
-// check `request.user.role` and decide if ok to continue 
-app.use("/api/private", permit("admin"));
-app.use(["/api/foo", "/api/bar"], permit("owner", "employee"));
-
-// setup requests handlers
-api.get("/private/whatever", (req, res) => response.json({whatever: true}));
-api.get("/foo", (req, res) => response.json({currentUser: req.user}));
-api.get("/bar", (req, res) => response.json({currentUser: req.user}));
-
-// setup permissions based on HTTP Method
-
-// account creation is public
-api.post("/account", (req, res) => req.json({message: "created"}));
-
-// account update & delete (PATCH & DELETE) are only available to account owner
-api.patch("/account", permit('owner'), (req, res) => req.json({message: "updated"}));
-api.delete("/account", permit('owner'), (req, res) => req.json({message: "deleted"}));
-
-// viewing account "GET" available to account owner and account member
-api.get("/account", permit('owner', 'employee'),  (req, res) => req.json({currentUser: request.user}));
-
-// mount api router
-app.use("/api", api);
-
-// start 'er up
-app.listen(process.env.PORT || 3000);
-// middleware for doing role-based permissions
-export default function permit(...allowed) {
-  const isAllowed = role => allowed.indexOf(role) > -1;
-
-  // return a middleware
-  return (req, res, next) => {
-    if (req.user && isAllowed(req.user.role))
-      next(); // role is allowed, so continue on the next middleware
-    else {
-      response.status(403).json({message: "Forbidden"}); // user is forbidden
-    }
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database: ', err);
+    return;
   }
-}
-export default function loadDb(req, res, next) {
+  console.log('Connected to MySQL database');
+});
 
-  // dummy db
-  request.db = {
-    users: {
-      findByApiKey: async token => {
-        switch {
-          case (token == '1234') {
-            return {role: 'superAdmin', id: 1234};
-          case (token == '5678') {
-            return {role: 'admin', id: 5678};
-          case (token == '1256') {
-            return {role: 'editor', id: 1256};
-          case (token == '5621') {
-            return {role: 'user', id: 5621};
-          default:
-            return null; // no user
-        }
-      }
-    }
-  };
+// Define the routes for sign up and sign in
+app.post('/signup', (req, res) => {
+  // Implement role-based authorization logic for sign up
+  // ...
+  res.send('Sign up successful');
+});
 
-  next();
-}
-export default async function authorize(req, res, next) {
-  const apiToken = req.headers['x-api-token'];
+app.post('/signin', (req, res) => {
+  // Implement role-based authorization logic for sign in
+  // ...
+  res.send('Sign in successful');
+});
 
-  // set user on-success
-  request.user = await req.db.users.findByApiKey(apiToken);
-
-  // always continue to next middleware
-  next();
-}
+// Start the server
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
